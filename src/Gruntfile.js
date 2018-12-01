99a3e2358143a4aea7391db603d8783d64e2f759
Mike McAllister <mike@logikbox.com>
2018-12-01 15:23:22 -0500

    +bootstrap, +grunt

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -1,1 +1,80 @@
-# -
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+    // Tasks
+    sass: { // Begin Sass Plugin
+      dist: {
+        options: {
+          sourceMap : true
+        },
+        files: [{
+          expand: true,
+          cwd: 'scss',
+          src: ['**/*.scss'],
+          dest: 'css',
+          ext: '.css'
+      }]
+      }
+    },
+    postcss: { // Begin Post CSS Plugin
+      options: {
+        map: true,
+        processors: [
+      require('autoprefixer')({
+            browsers: ['last 2 versions']
+          })
+    ]
+      },
+      dist: {
+        src: 'css/styles.css'
+      }
+    },
+    cssmin: { // Begin CSS Minify Plugin
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+    }]
+      }
+    },
+    uglify: { // Begin JS Uglify Plugin
+      build: {
+        src: ['javascript/*.js'],
+        dest: 'js/script.min.js'
+      }
+    },
+    watch: { // Compile everything into one task with Watch Plugin
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass', 'postcss', 'cssmin']
+      },
+      js: {
+        files: '**/*.js',
+        tasks: ['uglify']
+      }
+    }
+	});
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', [
+    'watch',
+    'browserify',
+    'uglify'
+  ]);
+
+};
+
---
+++ src/javascript/ui-common.js
@@ -0,0 +1,32 @@
+ if (typeof (Logikbox) === 'undefined' || Logikbox === null) {
+        Logikbox = {};
+    }
+    if (typeof (Logikbox.Common) === 'undefined' || Logikbox.Common === null) {
+        Logikbox.Foo = {};
+    }
+    (function (rootNamespace, $) {
+        //namesaoce code here
+
+         this.go = function(e) {
+                return e + ": here";
+
+            };
+        this.alertme = function(e){
+            alert(e);
+            };
+
+
+        $(document).ready(function(){
+
+            console.log("in namespoace");
+
+        });
+
+        //list of methods to map
+        rootNamespace.alertme = this.alertme;
+        rootNamespace.go = this.go;
+    })(Logikbox.Foo, jQuery);
+
+
+    console.log(Logikbox.Common);
+    console.log(Logikbox.Common.go("hello Mike"));
